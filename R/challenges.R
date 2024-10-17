77c3ac357c8a47da5f9fe670beab4b97e270947c
Nuwani Palihawadana <60213950+nuwani-palihawadana@users.noreply.github.com>
2024-10-17 14:26:29 +1100

    Improve func1

--- R/challenges.R
+++ R/challenges.R
@@ -1,21 +1,27 @@
 #' @title First function
 #' @description
 #' Given a square matrix, calculates the average over the sum of row averages and column averages
-#' 
+#'
 #' @param m a square matrix with no missing values
 #' @return Single numerical value
 #' @examples
 #' m <- matrix(seq(16),nrow=4)
 #' un(m)
 #' @export
+# un <- function(m) {
+#   mean(apply(m,1,mean) + apply(m,2,mean))
+# }
+
 un <- function(m) {
-  mean(apply(m,1,mean) + apply(m,2,mean))
+  rM <- rowMeans(m)
+  cM <- colMeans(m)
+  mean(rM + cM)
 }
 
 #' @title Second
 #' @description
 #' Given a vector gives the longest continuous increasing subset
-#' 
+#'
 #' @param vec Numerical vector with no missing values
 #' @return A numerical vector containing the longest continuous increasing subset
 #' @export
@@ -39,14 +45,14 @@ deux <- function(vec) {
   if (length(current_seq) > length(longest_seq)) {
     longest_seq <- current_seq
   }
-  
+
   return(longest_seq)
 }
 
 #' @title Third
 #' @description
 #' Given a vector return the count of each unique element
-#' 
+#'
 #' Hint: Try looking into `tabulate`, `fastmatch::fastmatch`
 #' @param vec Numerical vector
 #' @return A single numerical vector with counts of each unique element
